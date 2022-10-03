8b4b646c30f625a6c8e194b7b9a0e841d4a56334
Sergey Chuguy <mr.chuguy@gmail.com>
2022-10-03 20:26:51 +0300

    finish first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBut = document.querySelector('button[data-start]');
+const stopBut = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+stopBut.disabled = true;
+
+const startBgColorChange = () => {
+  changeButtonsState();
+  body.style.background = getRandomHexColor();
+  intervalId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+};
+
+const stopBgColorChange = () => {
+  changeButtonsState();
+  clearInterval(intervalId);
+};
+
+const changeButtonsState = () => {
+  startBut.disabled = !startBut.disabled;
+  stopBut.disabled = !stopBut.disabled;
+};
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBut.addEventListener('click', startBgColorChange);
+stopBut.addEventListener('click', stopBgColorChange);
